bd9ccbb2baaea29a8979f465a71a411c28346f7b
Jør∂¡ <itsbrickpop@gmail.com>
2021-03-14 00:06:32 +0000

    Serve working

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.5.0
 	github.com/gofiber/websocket/v2 v2.0.2
 	github.com/klauspost/compress v1.11.12 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/savsgio/gotils v0.0.0-20210225112730-595c7e5a8a7a // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
--- services/server.go
+++ services/server.go
@@ -8,6 +8,7 @@ import (
 
 	fiber "github.com/gofiber/fiber/v2"
 	websocket "github.com/gofiber/websocket/v2"
+	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
 
@@ -121,7 +122,7 @@ func handleGet(ctx *fiber.Ctx) error {
 
 	found := false
 	for i := 0; i < len(listeners); i++ {
-		for j := 0; i < len(listeners[i].actions); j++ {
+		for j := 0; j < len(listeners[i].actions); j++ {
 			if listeners[i].actions[j].name == action {
 				found = true
 				log.Println("Relaying", action, "to", listeners[i].name)
@@ -138,7 +139,6 @@ func handleGet(ctx *fiber.Ctx) error {
 		return ctx.SendString("Not found")
 	}
 
-	log.Println("TO DO: Handle request", action, token)
 	return nil
 }
 
@@ -157,12 +157,12 @@ func handleWsClient(c *websocket.Conn) {
 	// c.Locals is added to the *websocket.Conn
 	if c.Locals("allowed") != true {
 		log.Println("Closing non-upgraded connection")
-		c.WriteMessage(1, []byte(`{"error":true,"message":"Please, upgrade to web sockets"}`))
+		c.WriteMessage(1, []byte(`{"ok":false,"message":"Please, upgrade to web sockets"}`))
 		c.Close()
 		return
 	} else if c.Params("token") != viper.GetString("token") {
 		log.Println("Unauthorized token", c.Params("token"))
-		c.WriteMessage(1, []byte(`{"error":true,"message":"Unauthorized"}`))
+		c.WriteMessage(1, []byte(`{"ok":false,"message":"Unauthorized"}`))
 		c.Close()
 		return
 	}
@@ -180,17 +180,23 @@ func handleWsClient(c *websocket.Conn) {
 			log.Println("[read]", err)
 			break
 		}
-		// log.Printf("recv: %s", msg)
 
 		var decodedMessage ListenerMessage
 		if err := json.Unmarshal(msg, &decodedMessage); err != nil {
 			log.Println("[read] Could not unmarshall the message", err)
+			c.WriteMessage(msgType, []byte(`{"ok":false}`))
+			c.Close()
 			break
 		}
 
 		switch decodedMessage.Command {
 		case "register":
-			registerListener(decodedMessage, c, msgType, fmt.Sprintf("%v", c.Locals("IP")))
+			err := registerListener(decodedMessage, c, msgType, fmt.Sprintf("%v", c.Locals("IP")))
+			if err != nil {
+				c.WriteMessage(msgType, []byte(fmt.Sprintf(`{"ok":false,"message":"%s"}`, err.Error())))
+			} else {
+				c.WriteMessage(msgType, []byte(`{"ok":true}`))
+			}
 			break
 		default:
 			log.Println("[read] Unrecognized command", decodedMessage.Command)
@@ -203,11 +209,19 @@ func handleWsClient(c *websocket.Conn) {
 	}
 
 	log.Println("Disconnected", c.Locals("IP"))
+	unregisterListener(c)
 }
 
 // Helpers
 
-func registerListener(decodedMessage ListenerMessage, connection *websocket.Conn, msgType int, ip string) {
+func registerListener(decodedMessage ListenerMessage, connection *websocket.Conn, msgType int, ip string) error {
+	// Check for duplicates
+	for i := 0; i < len(listeners); i++ {
+		if listeners[i].name == decodedMessage.Name {
+			return errors.Errorf("A service with the same name is already registered")
+		}
+	}
+
 	actions := make([]struct {
 		name   string
 		status ActionStatus
@@ -228,6 +242,35 @@ func registerListener(decodedMessage ListenerMessage, connection *websocket.Conn
 	listeners = append(listeners, newListener)
 
 	log.Printf("[%s] Registered %s", decodedMessage.Name, ip)
+	return nil
+}
+
+func unregisterListener(c *websocket.Conn) {
+	name := ""
+	ip := ""
+
+	// Remove from the list
+	idx := -1
+	for i := 0; i < len(listeners); i++ {
+		if listeners[i].connection == c {
+			idx = i
+			name = listeners[i].name
+			ip = listeners[i].ip
+			c.Close()
+			break
+		}
+	}
+
+	if idx < 0 {
+		log.Println("[err] Listener item not found")
+		return
+	}
+
+	if len(listeners) > 0 {
+		listeners[len(listeners)-1], listeners[idx] = listeners[idx], listeners[len(listeners)-1]
+	}
+	listeners = listeners[:len(listeners)-1]
+	log.Printf("[%s] Unregistered %s", name, ip)
 }
 
 func notifyListener(listener Listener, action string, token string) {
